b66364615175881e7ccb5cd0ac4296ad22ccd162
José López <jose.lopez100@upr.edu>
2020-10-28 00:59:35 -0400

    Implemented Mult. by 4/Sign Extender with Test

---
+++ X4_Sign_Extender.v
@@ -0,0 +1,27 @@
+module x4_Sign_Extender(output reg [31:0] extended, input [23:0] input_instr);
+always @ (*) begin
+    extended = {{8{1'b0}}, input_instr};
+    extended = extended * 4;
+end
+endmodule
+
+//Test
+module x4_Sign_Extender_test;
+//input
+reg [23:0] input_instr;
+
+//output
+wire [31:0] extended;
+
+//Intantiation
+x4_Sign_Extender unit (extended, input_instr);
+
+initial begin
+    input_instr = $random;
+end
+
+initial begin
+    $display ("Input(binary)                  Input(decimal)        Extended Output(binary)            Extended Output (decimal)");
+    $monitor ("%b      %d          %b       %d", input_instr, input_instr, extended, extended);
+end
+endmodule
